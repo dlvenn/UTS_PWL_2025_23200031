19ff21d3b2a6d0b62fd376b2d3a7d97f7027a516
dlvenn <desyanalavenia6@gmail.com>
2025-04-29 06:19:11 +0700

    api get & post selesai

--- src/app/api/preorder/route.js
+++ src/app/api/preorder/route.js
@@ -9,6 +9,7 @@ export async function GET() {
     const formattedData = data.map((item) => ({
         id: item.id,
         order_date: item.order_date.toISOString().split('T')[0],
+        order_by: item.order_by,
         selected_package: item.selected_package,
         qty: item.qty,
         status: item.is_paid ? "Lunas" : "Belum Lunas",
@@ -42,36 +43,3 @@ export async function POST(request) {
     return new Response(JSON.stringify(preorder), { status: 201 });
 }
 
-export async function PUT(request) {
-    const { id, order_date, order_by, selected_package, qty, status } = await request.json();
-    if (!id || !order_date || !order_by || !selected_package || !qty || !status) return new Response(JSON.stringify({ error: 'Field kosong' }), { status: 400 });
-
-    const validOrderDate = new Date(order_date).toISOString();
-
-    const is_paid = status === "Lunas";
-    
-    const preorder = await prisma.preorder.update({
-        where: { id },
-        data: { order_date: validOrderDate, order_by, selected_package, qty, is_paid },
-    });
-
-    // format tampilan hasil di Postman
-    const formattedPreorder = {
-        id: preorder.id,
-        order_date: preorder.order_date.toISOString().split('T')[0],
-        order_by: preorder.order_by,
-        selected_package: preorder.selected_package,
-        qty: preorder.qty,
-        status: preorder.is_paid ? "Lunas" : "Belum Lunas",
-    };
-
-    return new Response(JSON.stringify(formattedPreorder), { status: 200 });
-}
-
-export async function DELETE(request) {
-    const { id } = await request.json();
-    if (!id) return new Response(JSON.stringify({ error: "ID tidak ditemukan" }), { status: 400 });
-
-    await prisma.preorder.delete({ where: { id } });
-    return new Response(JSON.stringify({ message: "Berhasil dihapus" }), { status: 200 });
-}
