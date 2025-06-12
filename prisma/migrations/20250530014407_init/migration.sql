0634693ac2843c3b7aa52ef203cd211662c5bdaa
dlvenn <desyanalavenia6@gmail.com>
2025-06-12 10:29:07 +0700

    add table customer dan relation id paket ke preorder

---
+++ prisma/migrations/20250530014407_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "customer" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
